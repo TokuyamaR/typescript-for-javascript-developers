f50bbd882aa4d86abb1824874b198eaa8393b9d8
TokuyamaR <qpflapper.1007@gmail.com>
2021-02-16 09:24:53 +0900

    code any type and interface

--- src/any.ts
+++ src/any.ts
@@ -2,16 +2,23 @@ import axios from 'axios';
 
 export {};
 
-let url: string =
-  'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123';
 
-axios.get(url).then(function(response) {
-  interface Article {
-    id: number;
-    title: string;
-    description: string;
-  }
-  let data: Article[];
-  data = response.data;
-  console.log(data);
-});
+let url: string = 'https://udemy-utils.herokuapp.com/api/v1/articles?token=token123'
+
+axios.get(url)
+  .then(function (response) {
+    interface Article { // 独自の型を自作することができる
+      id: number,
+      title: string,
+      description: string,
+    }
+    let data: Article[] = response.data;
+    data = [
+      {
+        id: 1,
+        title: 'title',
+        description: 'description'
+      }
+    ]
+    console.log(data);
+  })
