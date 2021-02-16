b34106a9aca7fcaac192afe513be5f07b988eec9
TokuyamaR <qpflapper.1007@gmail.com>
2021-02-16 09:30:37 +0900

    code void type

--- src/void.ts
+++ src/void.ts
@@ -1,7 +1,7 @@
 export {};
 
-function returnNothing(): void {
-  console.log("I dont't return anything!");
+function returnNothing(): void{
+  console.log("I don't return anything!");
 }
 
-console.log(returnNothing());
+console.log(returnNothing)
